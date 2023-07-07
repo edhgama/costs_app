5d23ce498558f4f69fa8cb2702f91724f9faf5e4
Edheyberg Gama <edhgama@gmail.com>
2023-07-07 19:43:38 -0400

    fix texto onde nao devia

--- src/Components/Layout/Navbar.js
+++ src/Components/Layout/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar() {
           <li className={styles.item}><Link to="/">Home</Link></li>
           <li className={styles.item}><Link to="/company">Empresa</Link></li>
           <li className={styles.item}><Link to="/contact">Contato</Link></li>
-          <li className={styles.item}><Link to="/projects">Novo Projeto</Link></li>
+          <li className={styles.item}><Link to="/projects">Projetos</Link></li>
       </ul>
     </Container>
     </nav >
