219280e8eacfbeb97c5dc1d3bc3ce9331110ed83
Edheyberg Gama <edhgama@gmail.com>
2023-07-08 02:11:46 -0400

    json-server categorias submit

--- src/Components/Projects/ProjectForm.js
+++ src/Components/Projects/ProjectForm.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
+
 import Input from "../Form/Input";
 import Select from "../Form/Select";
 import SubmitButton from "../Form/SubmitButton";
 
 import styles from "./ProjectForm.module.css";
 
-export default function ProjectForm({btnText}) {
+export default function ProjectForm({ btnText }) {
+
+  const [categories, setCategories] = useState([])
+
+  useEffect(() => { 
+      fetch("http://localhost:5000/categories",
+    {
+      method: "GET",
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    .then((resp) => resp.json())
+    .then((data) => {setCategories(data)
+      })
+    .catch((err)=> console.log(err))
+  },[]
+  )
+
   return (
     <form className={styles.forms}>
       <Input
@@ -12,16 +32,17 @@ export default function ProjectForm({btnText}) {
         text="Nome do projeto"
         name="name"
         placeholder="Insira Nome do Projeto" />
-     <Input
+      <Input
         type="number"
         text="Orçamento do projeto"
         name="budget"
         placeholder="Insira Valor do Projeto" />
       <Select
         name="category_id"
-        text="Selecione uma Categoria" 
+        text="Selecione uma Categoria"
+        options={categories}
       />
-      <SubmitButton text={btnText}/> 
+      <SubmitButton text={btnText} />
     </form>
   )
 }
