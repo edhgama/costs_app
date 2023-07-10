e8c94a5e8b165b6bc8fc90a14bf8d99d9c97f229
Edheyberg Gama <edhgama@gmail.com>
2023-07-09 23:14:59 -0400

    Dashboard titulos+ placeholder

--- src/Components/Pages/Projects.js
+++ src/Components/Pages/Projects.js
@@ -1,6 +1,9 @@
 import {useLocation} from 'react-router-dom'
+import LinkButton from '../Layout/LinkButton'
+import Container from '../Layout/Container'
 
 import Message from "../Layout/Message"
+import styles from "./Projects.module.css"
 
 export default function Projects() {
 
@@ -11,10 +14,17 @@ export default function Projects() {
   }
 
 
+
   return (
-    <div>
-      <h1>Projects</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to="/newproject" text="Criar Projeto"/>
+      </div>
       {message && <Message msg={message} type={'sucess'}/> }
+      <Container customClass="start">
+        <p>Projetos... </p>
+      </Container>
     </div>
   )
 
